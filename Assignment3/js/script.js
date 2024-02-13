e025fe7f382090343f2f2657d6860017f5ce39ae
VioletZumwalt <vzumwalt02@gmail.com>
2024-02-12 19:33:43 -0700

    homework

--- Assignment3/js/script.js
+++ Assignment3/js/script.js
@@ -1,38 +1,77 @@
-let catImg;
-let x, y;
-let xSpeed, ySpeed;
+// Load the images
+let jumpCatImage;
+let chillCatImage;
+let aiCatImage;
+
+var Font;
 
 function preload() {
-  catImg = loadImage('Images/Cat-Selfie.png');
+  jumpCatImage = loadImage('images/Cat-Jump.png');
+  chillCatImage = loadImage('images/Cat-Chill.png');
+  aiCatImage = loadImage('images/AI-Cat.png');
+  Font = loadFont('fonts/Kingsman.ttf');
 }
+
+// Chill speeds and locale
+let chillX, chillY; 
+let chillSpeedX, chillSpeedY; 
+
+// Jump Locale
+let jumpX, jumpY; 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  x = random(width);
-  y = random(height);
-  xSpeed = random(1, 3);
-  ySpeed = random(1, 3);
-}
+  createCanvas(800, 600); 
+  chillX = width / 2;
+  chillY = height / 2;
+  chillSpeedX = random(-5, 5); 
+  chillSpeedY = random(-5, 5); 
+  jumpX = width / 2;
+  jumpY = height / 2;
 
-let myFont;
-function preload() {
-  myFont = loadFont('fonts/Kingsman.ttf');
-}
-
-function setup() {
-  fill('#ED225D');
-  textFont(myFont);
-  textSize(36);
-  text('p5*js', 10, 50);
-}
-
-var x = 0;
-function setup() {
-  createCanvas(400, 400);
+  // Move Time (5 sec?)
+  setInterval(moveJumpCat, 5000);
 }
 
 function draw() {
-  background(220);
-  square(x,100,25);
-  x++;
+  background(255);
+
+  // Chill locale
+  chillX += chillSpeedX;
+  chillY += chillSpeedY;
+  
+  // Wall jump
+  if (chillX <= 0 || chillX >= width) {
+    chillSpeedX *= -1;
+  }
+  if (chillY <= 0 || chillY >= height) {
+    chillSpeedY *= -1;
+  }
+  
+  // Cat Spawner
+  image(chillCatImage, chillX, chillY);
+  
+  image(aiCatImage, 200, 200);
+
+  image(jumpCatImage, jumpX, jumpY);
+  
+  // Border
+  noStroke();
+  fill(150, 200, 255);
+  rect(0, 0, 800, 25);
+  rect(0, 25, 25, 600);
+  rect(25, 575, 800, 25);
+  rect(775, 25, 25, 600);
+  fill(0);
+  
+  // Font
+  textFont(newFont);
+  textSize(25);
+  text("Violet Zumwalt", 500, 525);
+  text("Cat Images", 100, 75);
+}
+
+// Jump Move
+function moveJumpCat() {
+  jumpX = random(width);
+  jumpY = random(height);
 }
