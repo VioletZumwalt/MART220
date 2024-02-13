1d360130e63079d2f79e319843fcb48593641969
VioletZumwalt <vzumwalt02@gmail.com>
2024-02-12 18:29:43 -0700

    homeowkr

--- Assignment3/js/script.js
+++ Assignment3/js/script.js
@@ -25,3 +25,14 @@ function setup() {
   textSize(36);
   text('p5*js', 10, 50);
 }
+
+var x = 0;
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  square(x,100,25);
+  x++;
+}
